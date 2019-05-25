e7e3ae712bbb564a86663e609c0e3a469eb70a2c
heru <heru@helios.local>
2019-05-25 10:51:17 +0700

    fix pages

--- app_server/controllers/main.js
+++ app_server/controllers/main.js
@@ -1,9 +1,31 @@
 const locationList = (req, res) => {
   res.render('location-list', {
-    title: 'List of location'
+    pageHeader: {
+      title: 'List Location',
+      description: 'all you can eat'
+    },
+    locations: [{
+      place: 'Starcups',
+      rating: 3,
+      address: '125 High street, Reading, RG12 1PS',
+      facilities: ['Hot Drinks', 'Foods', 'Premium Wifi']
+    }, {
+        place: 'Barbeque',
+        rating: 4,
+        address: '125 Low street, Reading, RG12 1PS',
+        facilities: ['Hot Drinks', 'Foods', 'Premium Wifi']  
+    }]
   })
 }
 
+const location = (req, res) => {
+  res.render('location', {title: 'Location'})
+}
+const reviewAddForm = (req, res) => {
+  res.render('review-add-form', {title: 'Add Location Review'})
+}
 module.exports = {
   locationList,
+  location,
+  reviewAddForm
 }
--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,8 +1,13 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
 const mainCtrl = require('../controllers/main');
+const otherCtrl = require('../controllers/others');
+
+const router = express.Router();
 
 /* GET home page. */
 router.get('/', mainCtrl.locationList);
+router.get('/about', otherCtrl.about);
+router.get('/location', mainCtrl.location)
+router.get('/location/review/new', mainCtrl.reviewAddForm)
 
 module.exports = router;
